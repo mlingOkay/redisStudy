5c0a94016f334eb0250cecd2b351e53a29727f9c
mlNothing <mlingokay@gmail.com>
2022-08-20 16:05:54 +0800

    test

---
+++ src/main/java/com/example/redisstudy/jedis/JedisDemo01.java
@@ -0,0 +1,13 @@
+package com.example.redisstudy.jedis;
+
+/**
+ * @Author: mlingOkay
+ * @CreateTime: 2022-08-20  16:05
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class JedisDemo01 {
+    public static void main(String[] args) {
+
+    }
+}
