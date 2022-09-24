8dab195d921444fb0a275d86450b05955145db3d
mlNothing <mlingokay@gmail.com>
2022-09-24 20:36:45 +0800

    test

--- src/main/java/com/example/redisstudy/jedis/controller/JedisDemo01.java
+++ src/main/java/com/example/redisstudy/jedis/controller/JedisDemo01.java
@@ -1,4 +1,4 @@
-package com.example.redisstudy.jedis;
+package com.example.redisstudy.jedis.controller;
 
 import redis.clients.jedis.Jedis;
 
---
+++ src/main/java/com/example/redisstudy/jedis/controller/RedisKill01.java
@@ -0,0 +1,16 @@
+package com.example.redisstudy.jedis.controller;
+
+/**
+ * @Author: mlingOkay
+ * @CreateTime: 2022-08-29  12:44
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class RedisKill01 {
+    public static void main(String[] args) {
+
+    }
+    public static  boolean doSecKill(){
+        return false;
+    }
+}
