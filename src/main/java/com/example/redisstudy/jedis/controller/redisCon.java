5135f243c0c32b7fce5fae1003c7fe69c96fa914
mlNothing <mlingokay@gmail.com>
2022-09-25 16:55:06 +0800

    uuid防误删操作

--- src/main/java/com/example/redisstudy/jedis/controller/redisCon.java
+++ src/main/java/com/example/redisstudy/jedis/controller/redisCon.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Author: mlingOkay
  * @CreateTime: 2022-08-23  19:43
@@ -29,8 +32,9 @@ public class redisCon {
     }
     @GetMapping("testLock")
     public void testLock(){
-        //1获取锁，setne 类似于 setnx lock 111
-        Boolean lock = redisTemplate.opsForValue().setIfAbsent("lock", "111");
+        //声明一个uuid,将作为一个value放入我们的key所对应的值中
+        String uuid = UUID.randomUUID().toString();
+        Boolean lock = redisTemplate.opsForValue().setIfAbsent("lock", "111",3,TimeUnit.SECONDS);
         //2获取锁成功、查询num的值
         if (lock) {
             Object value = redisTemplate.opsForValue().get("num");
@@ -38,13 +42,15 @@ public class redisCon {
             if (StringUtils.isEmpty(value)) {
                 return;
             }
+            //不是孔 如果说在这出现了异常 那么delete就删除失败 也就是说锁永远存在
             //2.2有值就转成成int
             int num = Integer.parseInt(value + "");
             //2.3把redis的num加1
-            redisTemplate.opsForValue().set("num", ++num);
-            //2.4释放锁，del
-            redisTemplate.delete("lock");
-
+            redisTemplate.opsForValue().set("num",++num);
+            if(uuid.equals((String)redisTemplate.opsForValue().get("lock"))){
+                //2.4释放锁，del
+                this.redisTemplate.delete("lock");
+            }
         } else {
             //3获取锁失败、每隔0.1秒再获取
             try {
