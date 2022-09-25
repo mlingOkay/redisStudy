0bfb66a50ffb89b0eec2a3447f79d24e0b4e5b43
mlNothing <mlingokay@gmail.com>
2022-09-25 13:46:01 +0800

    第一次ping

--- src/main/java/com/example/redisstudy/jedis/controller/TestPing.java
+++ src/main/java/com/example/redisstudy/jedis/controller/TestPing.java
@@ -4,7 +4,7 @@ import redis.clients.jedis.Jedis;
 public class TestPing {
 	public static void main(String[] args) 
 	{
-		Jedis jedis = new Jedis("192.168.124.7",6379);
+		Jedis jedis = new Jedis("192.168.227.2",6379);
 		System.out.println(jedis.ping());
 	}
 }
